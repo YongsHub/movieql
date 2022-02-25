490237c41ba67a8810d6541b0605bce1b2639792
TAEYONG KIM <amico741@naver.com>
2022-02-25 17:33:10 +0900

    .

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,39 @@
-const taeyong = {
-    name: "Taeyong",
-    age: 25,
-    gender: "male"
-};
+const person = [
+    {
+        name: "Taeyong",
+        age: 25,
+        gender: "male"
+    },
+    {
+        name: "Jaekwang",
+        age: 25,
+        gender: "male"
+    },
+    {
+        name: "Rosa",
+        age: 25,
+        gender: "female"
+    },
+    {
+        name: "saeyun",
+        age: 25,
+        gender: "male"
+    },
+    {
+        name: "jeonghoo",
+        age: 25,
+        gender: "male"
+    },
+    {
+        name: "jayPark",
+        age: 25,
+        gender: "male"
+    },
+]
 
 const resolvers = {
     Query: {
-        person: () => taeyong
+        Persons: () => person
     }
 };
 
