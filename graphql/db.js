245b57a9b995386ebe7c35d1fcee625e75b9e87d
TAEYONG KIM <amico741@naver.com>
2022-02-26 15:39:37 +0900

    .

--- graphql/db.js
+++ graphql/db.js
@@ -1,6 +1,14 @@
 const Axios = require('axios');
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
 export const getMovies = async (limit, rating) => {
-    return await Axios.get(`${API_URL}`).then(res => res.data.data.movies);
+    let newAPI_URL = API_URL;
+    //return await Axios.get(`${API_URL}`).then(res => res.data.data.movies);
+    if(rating > 0){
+        newAPI_URL += `limit=${limit}`;
+    }
+    if(limit > 0){
+        newAPI_URL += `&minimum_rating=${rating}`;
+    }
+    return await Axios.get(`${newAPI_URL}`).then(res => res.data.data.movies);
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,6 +3,6 @@ import { getMovies } from "./db.js";
 
 export const resolvers = {
     Query: { // Read
-        Movies: () => getMovies()
+        Movies: (_, { limit, rating }) => getMovies(limit, rating)
     },
 };
